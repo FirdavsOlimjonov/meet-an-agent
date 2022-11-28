27e29aad1c62637e28b1039bad0ddc9102a5059f
Firdavs Olimjonov <firdavsolimjonov25@gmail.com>
2022-11-28 10:58:33 +0000

    Update MeetAnAgent.java

--- src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
+++ src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
@@ -1,4 +1,5 @@
 package com.epam.rd.autotasks.meetanagent;
+import java.util.Scanner;
 
 public class MeetAnAgent {
     final static int PASSWORD = 133976; //You can change pass, but don't change the type
